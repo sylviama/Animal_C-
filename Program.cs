d7d77fe0759d43aa2a227e42e8e344a43415f7ca
Sylvia Ma <xiangwma@umich.edu>
2016-07-13 20:49:25 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using zoo;
+
+namespace StartOOP
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Program...");
+            Animal my_animal = new Animal();//creates an instance of type "Animal"
+            my_animal.ToString();
+            Console.WriteLine(my_animal.ToString());
+            Console.WriteLine("The Animal is: " + my_animal.sleep());
+            Dog my_dog = new Dog();//bring a Dog to life
+            Console.WriteLine("This Dog is: " +my_dog.sleep());
+            Console.WriteLine("Ending Program...");
+        }
+    }
+}
---
+++ animal.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace zoo
+{
+    public class Animal{
+
+        public string sleep(){//this is a method
+            return "Zzzzzz...";
+        }
+
+    }
+
+    public class Dog: Animal{//anything put into Animal, now inheritence to Dog
+
+    }
+
+
+}
